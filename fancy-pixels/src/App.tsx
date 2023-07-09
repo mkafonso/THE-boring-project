1db4c9b0ed9db407f8c79a0f72f4ff3ebc61d919
makaya2afonso <makaya2afonso@gmail.com>
2023-07-09 15:05:27 -0300

    init project fancy-pixels with react and typescript

---
+++ fancy-pixels/src/App.tsx
@@ -0,0 +1,10 @@
+export function App() {
+  return (
+    <h1>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloribus
+      laborum ipsa cupiditate possimus necessitatibus magnam officia expedita
+      alias commodi distinctio. Dolorum animi possimus error dolor! Porro vitae
+      nobis dicta perspiciatis?
+    </h1>
+  );
+}
---
+++ fancy-pixels/tailwind.config.js
@@ -0,0 +1,37 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: "Assistant, sans-serif",
+      },
+      colors: {
+        "main-50" : "#fafafa",
+        "main-100": "#f4f4f5",
+        "main-200": "#e4e4e7",
+        "main-300": "#d4d4d8",
+        "main-400": "#a1a1aa",
+        "main-500": "#71717a",
+        "main-600": "#52525b",
+        "main-700": "#3f3f46",
+        "main-800": "#27272a",
+        "main-900": "#18181b",
+        "main-950": "#09090b",
+
+        "primary-50" : "#ecfdf5",
+        "primary-100": "#d1fae5",
+        "primary-200": "#a7f3d0",
+        "primary-300": "#6ee7b7",
+        "primary-400": "#34d399",
+        "primary-500": "#10b981",
+        "primary-600": "#059669",
+        "primary-700": "#047857",
+        "primary-800": "#065f46",
+        "primary-900": "#064e3b",
+        "primary-950": "#022c22",
+      },
+    },
+  },
+  plugins: [require("tailwind-scrollbar")],
+};
