7923d936ba14bd8597d15ac3b3c409dbd5ffa10c
makaya2afonso <makaya2afonso@gmail.com>
2023-07-10 01:47:17 -0300

    add balance to userContract

--- contract-nexus/contracts/user-contract.sol
+++ contract-nexus/contracts/user-contract.sol
@@ -6,6 +6,7 @@ contract UserContract {
         string username;
         string bio;
         address walletAddress;
+        uint256 balance;
     }
 
     mapping(address => User) public users;
@@ -15,7 +16,22 @@ contract UserContract {
     function createUser(string memory _username, string memory _bio) public {
         require(bytes(users[msg.sender].username).length == 0, "User already exists");
 
-        users[msg.sender] = User(_username, _bio, msg.sender);
+        users[msg.sender] = User(_username, _bio, msg.sender, 0); // Initialize balance to 0
         emit UserCreated(msg.sender, _username);
+    }
+
+    function getBalance() public view returns (uint256) {
+        return users[msg.sender].balance;
+    }
+
+    function deposit() public payable {
+        users[msg.sender].balance += msg.value;
+    }
+
+    function withdraw(uint256 _amount) public {
+        require(users[msg.sender].balance >= _amount, "Insufficient balance");
+
+        users[msg.sender].balance -= _amount;
+        payable(msg.sender).transfer(_amount);
     }
 }
