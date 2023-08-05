13b28d439d096c390a4eb96de31ab4be422ed1cf
makaya2afonso <makaya2afonso@gmail.com>
2023-08-05 20:16:16 -0300

    ui fixes

--- client/src/components/common/header.tsx
+++ client/src/components/common/header.tsx
@@ -11,17 +11,12 @@ export function Header() {
           </a>
 
           <div className="flex items-center gap-2 cursor-pointer text-main-700 hover:text-main-800 transition">
-            <span className="font-semibold">Challenges:</span>
-            <span className="font-bold text-primary-600">3</span>
-          </div>
-
-          <div className="flex items-center gap-2 cursor-pointer text-main-700 hover:text-main-800 transition">
             <span className="font-semibold">My Wallet:</span>
             <span className="font-bold text-primary-600">1000 BP</span>
           </div>
         </div>
 
-        <strong className="font-bold hover:text-main-200 transition cursor-pointer">
+        <strong className="font-bold text-primary-600 hover:text-primary-800 transition cursor-pointer">
           0xA8bCe1bd5Ff836959F3993D2c97eD3a943812524
         </strong>
       </div>
--- client/src/components/screens/home.tsx
+++ client/src/components/screens/home.tsx
@@ -1,14 +1,14 @@
 export function Home() {
   return (
     <div className="flex gap-8 relative">
-      <div className="w-80 h-[41rem] shadown-md rounded-md bg-main-800 flex flex-col p-4 fixed top-24">
+      <div className="w-80 h-[41rem] shadown-md rounded-md bg-main-800 flex flex-col p-4 fixed top-28">
         <span>It's</span>
         <span className="text-primary-300 font-semibold">
           Sunday, the 16th june 2023
         </span>
       </div>
 
-      <div className="flex-1 h-[200vh] ml-[22rem] mt-24 flex flex-col gap-4 mb-40">
+      <div className="flex-1 h-[200vh] ml-[22rem] flex flex-col gap-4 mb-40">
         <div className="flex gap-4">
           <div className="flex flex-col gap-4">
             <div className="flex gap-4">
